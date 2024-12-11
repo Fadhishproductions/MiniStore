969ff90d0ef5ac2f56ff1a6bc621ec75c802c1fa
Your Name <fadhishshaji@gmail.com>
2024-12-11 10:53:00 +0530

    delete api change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-require('dotenv').config()
+const dotenv =  require('dotenv')
+dotenv.config()
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("MongoDB connected successfully");
